73aabaa6c2a92836c88a28a03198d864fc862ef5
wolfkainus <alfredo.lobos.g@gmail.com>
2017-09-13 15:52:09 -0300

    Conteo Numeros Repetidos

--- script-dado.js
+++ script-dado.js
@@ -1,13 +1,35 @@
-//DICE RANDOM
 var contador = 0;
+var Resultados = [];
+var contadorNumeroDados = [];
+var conteo = 0;
+var conteoActual = 0;
+var resultadoActual = 0;
 
-var carasDado = prompt("Tipo de Dados (D6, D20, etc..)?");
+var carasDado = prompt("De cuantas Caras quieres tus Dados (6, 20, etc..)?");
 var carasDado = parseInt(carasDado);
-var numeroDados = prompt("Cuantos D6 quieres lanzar?");
+var numeroDados = prompt("Cuantos Dados quieres lanzar?");
 var numeroDados = parseInt(numeroDados);
 
 while (contador < numeroDados) {
     contador = contador +1;
+    contadorNumeroDados.push(contador);
     var Number = Math.floor(Math.random() * carasDado) + 1;
+    Resultados.push(Number);
     document.write("Resultado: " + Number + "<br>");
-}
+};
+document.write("Estos son tus numeros " + Resultados + "<br>");
+
+document.write("contador numero de dados " + contadorNumeroDados + "<br>");
+
+contadorNumeroDados.forEach(function(contadorNumeroDado) {
+    Resultados.forEach(function(Resultado) {
+        if (Resultado == contadorNumeroDado) {
+            conteoActual = conteoActual + 1;
+            resultadoActual = Resultado;
+        }
+    });
+    if (conteoActual != 0){
+        document.write("Tienes " + conteoActual + " resultados de " + resultadoActual + "<br>");
+        conteoActual = 0;
+    }
+});
